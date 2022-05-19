464fd655c6964dd41b3da59732a8056362d41b73
Duck1s <joeri.breedveld123@gmail.com>
2022-05-19 17:08:36 +0200

    initial commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/app.css">
+    <title>My Blog</title>
+</head>
+<body>
+<article>
+    <h1><a href="/post">My First Post</a></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium debitis est exercitationem harum ipsam laboriosam laudantium pariatur quasi repellendus. Deserunt placeat, sequi! Consequatur cumque, eos, illo iusto maiores necessitatibus nisi nobis possimus provident repellat, unde ut! Consequuntur dolores enim fugiat ipsum iure, labore minima natus, quae quibusdam quisquam sit veniam.</p>
+</article>
+<article>
+    <h1><a href="/post">My Second Post</a></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium debitis est exercitationem harum ipsam laboriosam laudantium pariatur quasi repellendus. Deserunt placeat, sequi! Consequatur cumque, eos, illo iusto maiores necessitatibus nisi nobis possimus provident repellat, unde ut! Consequuntur dolores enim fugiat ipsum iure, labore minima natus, quae quibusdam quisquam sit veniam.</p>
+</article>
+<article>
+    <h1><a href="/post">My Third Post</a></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium debitis est exercitationem harum ipsam laboriosam laudantium pariatur quasi repellendus. Deserunt placeat, sequi! Consequatur cumque, eos, illo iusto maiores necessitatibus nisi nobis possimus provident repellat, unde ut! Consequuntur dolores enim fugiat ipsum iure, labore minima natus, quae quibusdam quisquam sit veniam.</p>
+</article>
+</body>
+</html>
